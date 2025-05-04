f833ef322f100775543ab0d7294956ee9023f37a
nwebster02 <n8webster02@gmail.com>
2025-05-04 04:10:22 -0600

    maze generation complete, just need tokenizer varitions and ascii (maybe?)

--- generate_mazes.py
+++ generate_mazes.py
@@ -3,33 +3,56 @@ import os
 
 from maze_dataset import MazeDataset, MazeDatasetConfig
 from maze_dataset.generation import LatticeMazeGenerators
-from maze_dataset.tokenization import MazeTokenizerModular, PromptSequencers
+from maze_dataset.tokenization import (AdjListTokenizers,
+                                       CoordTokenizers, 
+                                       EdgePermuters, 
+                                       EdgeSubsets, 
+                                       MazeTokenizer, 
+                                       MazeTokenizerModular, 
+                                       PathTokenizers, 
+                                       PromptSequencers, 
+                                       StepSizes, 
+                                       StepTokenizers, 
+                                       TargetTokenizers, 
+                                       TokenizationMode, 
+                                       _TokenizerElement,)
 
 cfg: MazeDatasetConfig = MazeDatasetConfig(
 	name="test", # name is only for you to keep track of things
 	grid_n=5, # number of rows/columns in the lattice
-	n_mazes=30, # number of mazes to generate
+	n_mazes=31, # number of mazes to generate
 	maze_ctor=LatticeMazeGenerators.gen_dfs, # algorithm to generate the maze
     maze_ctor_kwargs=dict(do_forks=False), # additional parameters to pass to the maze generation algorithm
 )
 
 dataset: MazeDataset = MazeDataset.from_config(cfg)
 
-# Need to fill this out with various tokenization methods
-tokenizer_list = [MazeTokenizerModular()]
+maze_folder = "mazes"
+os.makedirs(maze_folder, exist_ok=True)
+
+tokenizer_list = [MazeTokenizerModular(prompt_sequencer = PromptSequencers.AOTP(coord_tokenizer=CoordTokenizers.UT(),
+                                                                                adj_list_tokenizer=AdjListTokenizers.AdjListCoord(pre=False, post=True, shuffle_d0=True,edge_subset=EdgeSubsets.ConnectionEdges(walls=False), edge_permuter=EdgePermuters.RandomCoords()),
+                                                                                target_tokenizer=TargetTokenizers.Unlabeled(post=False),
+                                                                                path_tokenizer=PathTokenizers.StepSequence(step_size=StepSizes.Singles(), step_tokenizers=(StepTokenizers.Coord(),), pre=False, intra=False, post=False))),
+                  MazeTokenizerModular(prompt_sequencer = PromptSequencers.AOTP(coord_tokenizer=CoordTokenizers.CTT(),
+                                                                                adj_list_tokenizer=AdjListTokenizers.AdjListCoord(pre=False, post=True, shuffle_d0=True,edge_subset=EdgeSubsets.ConnectionEdges(walls=False), edge_permuter=EdgePermuters.RandomCoords()),
+                                                                                target_tokenizer=TargetTokenizers.Unlabeled(post=False),
+                                                                                path_tokenizer=PathTokenizers.StepSequence(step_size=StepSizes.Singles(), step_tokenizers=(StepTokenizers.Coord(),), pre=False, intra=False, post=False)))
+                 ]
 
 for tokenizer in tokenizer_list:
-    mazes = []
+    maze_list = []
     for maze in dataset.mazes:
-        maze = maze.as_tokens(maze_tokenizer=tokenizer)
-        mazes.append(maze)
+        maze_list.append(maze.as_tokens(maze_tokenizer=tokenizer))
 
-    maze_folder = "mazes"
-    os.makedirs(maze_folder, exist_ok=True)
-    tokenizer_folder = os.path.join(maze_folder, tokenizer.__class__.__name__) # Name untested
+    tokenizer_folder = os.path.join(maze_folder, tokenizer.prompt_sequencer.coord_tokenizer.__class__.__name__ + "_" + tokenizer.prompt_sequencer.adj_list_tokenizer.__class__.__name__ + "_" + tokenizer.prompt_sequencer.path_tokenizer.__class__.__name__)
     os.makedirs(tokenizer_folder, exist_ok=True)
 
-    for i, maze in enumerate(mazes):
-        maze_file = os.path.join(tokenizer_folder, f"maze_{i + 1}.txt")
+    for i, maze in enumerate(maze_list):
+        if i == 0:
+            maze_file = os.path.join(tokenizer_folder, "maze_example.txt")
+        else:
+            maze_file = os.path.join(tokenizer_folder, f"maze_{i}.txt")
+
         with open(maze_file, "w") as f:
-            f.write(maze)
+            f.write(" ".join(maze))
